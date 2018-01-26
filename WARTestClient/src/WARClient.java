88b0d7df1fe6227145146dd4ec926c1c1a6bab21
DarraghMc77 <mccaffrd@tcd.ie>
2018-01-26 11:29:11 +0000

    Some bug fixes

--- WARTestClient/src/WARClient.java
+++ WARTestClient/src/WARClient.java
@@ -7,36 +7,64 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.Charset;
+import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 
 public class WARClient {
-
+    
     public static void main(String[] args) throws Exception {
-        Socket socket = new Socket("localhost", 8011);
-        OutputStream outputStream = socket.getOutputStream();
-
-        BufferedImage image = ImageIO.read(new File("/Users/Darragh/eclipse-workspace/WARTestClient/src/dog.jpg"));
-
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        ImageIO.write(image, "jpg", byteArrayOutputStream);
-
-        byte[] size = ("SIZE " + Integer.toString(byteArrayOutputStream.size())).getBytes( Charset.forName("UTF-8"));
-        System.out.println(size);
-        outputStream.write(size);
-        outputStream.write(byteArrayOutputStream.toByteArray());
-        outputStream.flush();
-        System.out.println("Flushed: " + System.currentTimeMillis());
-
-        Thread.sleep(12000);
-        DataInputStream is = new DataInputStream(socket.getInputStream());
-        BufferedReader in = new BufferedReader(new InputStreamReader(is));
-        System.out.println(in.readLine());
-//		TODO: Deserialize json into object
-        in.close();
         
-        Thread.sleep(120000);
-        System.out.println("Closing: " + System.currentTimeMillis());
-        socket.close();
+        long start1 = System.currentTimeMillis();
+        long count = 1;
+        Socket socket = new Socket("192.168.6.131", 8046);
+        long average = 0;
+        
+        try {
+            while(true) {
+                long start2 = System.currentTimeMillis();
+                //                    System.out.println("top of while");
+                OutputStream outputStream = socket.getOutputStream();
+                BufferedImage image = ImageIO.read(new File("/Users/Darragh/eclipse-workspace/WARTestClient/src/dog.jpg"));
+                
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                ImageIO.write(image, "jpg", byteArrayOutputStream);
+                
+                byte[] size = ("SIZE " + Integer.toString(byteArrayOutputStream.size())).getBytes(Charset.forName("UTF-8"));
+                System.out.println(size);
+                outputStream.write(size);
+                outputStream.write(byteArrayOutputStream.toByteArray());
+                outputStream.flush();
+                System.out.println("Flushed: " + System.currentTimeMillis());
+                
+                //                Thread.sleep(12000);
+                DataInputStream is = new DataInputStream(socket.getInputStream());
+                BufferedReader in = new BufferedReader(new InputStreamReader(is));
+                System.out.println(in.readLine());
+                average = (start2 - System.currentTimeMillis());
+                System.out.println("time: " + (average));
+                //                count += 1;
+                //                TODO: Deserialize json into object
+                //                 in.close();
+                
+                Scanner reader = new Scanner(System.in);  // Reading from System.in
+                System.out.println("Enter a number: ");
+                int n = reader.nextInt();
+                if(n==1) {
+                    break;
+                }
+                
+            }
+        }
+        
+        finally{
+            System.out.println("Closing: " + System.currentTimeMillis());
+            socket.close();
+        }
+        
+        
+        //        Thread.sleep(120000);
+        
     }
+    
 }
--- WARTestServer/yolo_server.py
+++ WARTestServer/yolo_server.py
@@ -11,72 +11,83 @@ CONN_QUEUE = 50
 def main():
     # options = {"model": "cfg/yolo.cfg", "load": "bin/yolo.weights", "threshold": 0.1}
     options = {"model": "cfg/yolo.cfg", "load": "bin/yolo.weights", "threshold": 0.1, "gpu": 1.0}
-
+    
     tfnet = TFNet(options)
-
+    
     host = "192.168.6.126"
-    port = 8033
+    port = 8046
     print "Server Running on port: ", port
-
+    
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
     sock.bind((host, port))
     sock.listen(CONN_QUEUE)
-
-    while True:
-        print >> sys.stderr, 'waiting for a connection'
-        connection, client_address = sock.accept()
-
-        try:
-            print >> sys.stderr, 'connection from', client_address
+    
+    print >> sys.stderr, 'waiting for a connection'
+    connection, client_address = sock.accept()
+    
+    try:
+        print >> sys.stderr, 'connection from', client_address
+        
+        while True:
+            
             img = ""
             data = connection.recv(4096)
-            # try:
-            txt = data.decode('utf-8')
-            amount_received = 0
-
-            # Get size of image byte[]
-            if txt.startswith("SIZE"):
-                size = int(txt.split()[1])
-                print(size)
-            while True:
-                # print(amount_received)
-                data = connection.recv(4096)
-                amount_received += len(data)
-                img += data
-                if(amount_received >= size):
-                    break
-            print("Image received")
-
-            start = time.time()
-            nparr = numpy.fromstring(img, numpy.uint8)
-            img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-            # fh = open("./testImage.jpg", "wb")
-            # fh.write(img)
-            # fh.close()
-
-            # Run YOLO prediction
-            # imgcv = cv2.imread("./testImage.jpg")
-            result = tfnet.return_predict(img_np)
-            print(result)
-
-            print(start - time.time())
-
-            for obj in result:
-                print(type(obj['confidence']))
-                obj['confidence'] = str(obj['confidence'])
-
-            print str(result[0])
-
-            # only returning first one
-            json_result = json.dumps(result[0])
-            print(json_result)
-            connection.send(json_result)
-            # except Exception:
-            #     print(Exception.message)
-
-        finally:
-            connection.close()
+            print(data)
+            try:
+                start = time.time()
+                txt = data.decode('utf-8')
+                amount_received = 0
+                
+                # Get size of image byte[]
+                if txt.startswith("SIZE"):
+                    size = int(txt.split()[1])
+                    print(size)
+                while True:
+                    # print(amount_received)
+                    data = connection.recv(4096)
+                    print(data)
+                    amount_received += len(data)
+                    img += data
+                    if(amount_received >= size):
+                        break
+                print(start - time.time())
+                print("Image received")
+                
+                start = time.time()
+                nparr = numpy.fromstring(img, numpy.uint8)
+                img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+                
+                fh = open("./testImage.jpg", "wb")
+                fh.write(img)
+                fh.close()
+                
+                # Run YOLO prediction
+                imgcv = cv2.imread("./testImage.jpg")
+                result = tfnet.return_predict(imgcv)
+                print(result)
+                
+                print(start - time.time())
+                
+                # todo confidence is float check about converting that to json
+                for obj in result:
+                    obj['confidence'] = str(obj['confidence'])
+                
+                if result:
+                    print(result)
+                    # only returning first one
+                    json_result = json.dumps(result[0])
+                    print(json_result[0])
+                    connection.sendall(json_result+"\r\n")
+                    else:
+                    print("no result")
+                        connection.sendall("\r\n")
+                            except Exception as e:
+                                print("exception")
+                                connection.sendall("\r\n")
+                            finally:
+                                print("closing")
+                                connection.close()
 
 if __name__ == '__main__':
     main()
